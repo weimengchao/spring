dc9ca6c60baf4e1d3e91931b99d84f2eefab02be
weimengchao <weimengchao@songxiaocai.com>
2017-02-10 10:19:59 +0800

    a-service package fix

--- spring-cloud-demo/a-service/src/main/java/com/weimengchao/springCloudDemo/aService/AServiceApplication.java
+++ spring-cloud-demo/a-service/src/main/java/com/weimengchao/springCloudDemo/aService/AServiceApplication.java
@@ -1,4 +1,4 @@
-package com.weimengchao.springCloudDemo.eureka;
+package com.weimengchao.springCloudDemo.aService;
 
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.builder.SpringApplicationBuilder;
