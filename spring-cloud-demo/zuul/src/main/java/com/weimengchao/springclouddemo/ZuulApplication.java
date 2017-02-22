e2caf866d3170cbd7805856ee4f9140a76d162d8
wmc <weimengchao@songxiaocai.com>
2017-02-22 21:08:18 +0800

    services mapping

--- spring-cloud-demo/zuul/src/main/java/com/weimengchao/springclouddemo/ZuulApplication.java
+++ spring-cloud-demo/zuul/src/main/java/com/weimengchao/springclouddemo/ZuulApplication.java
@@ -1,9 +1,11 @@
 package com.weimengchao.springclouddemo;
 
 import org.springframework.boot.SpringApplication;
-import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.SpringCloudApplication;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
-@SpringBootApplication
+@EnableZuulProxy
+@SpringCloudApplication
 public class ZuulApplication {
 
 	public static void main(String[] args) {
